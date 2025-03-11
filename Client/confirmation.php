744f2f2de4f90bc8a29ee05d513fe5c3dcf9ff81
Ilias Bellir <belliri@formation.univ-smb.fr>
2025-03-11 08:49:40 +0100

    initialisation du confirmation client

---
+++ Client/confirmation.php
@@ -0,0 +1,28 @@
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>TranspLac</title>
+  <link rel="stylesheet" href="confimation_style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+  <img src="img/Logo.png" />
+  <div class="text">
+    <h1>Voici la date que vous avez sélectioner</h1>
+  </div>
+  <div class="date">
+		<p>Lundi 3 mars<p>
+		<p>Matin: Entre 8h et 10h<p>
+
+    
+  <div class="T_colis">
+    
+    <h1>Voulez vous confirmer cette date ?</h1>
+
+    <button class="button" type="submit"> Confirmer</button>
+    <button class="button" type="submit"> Rerourner à la page des Horraires</button>
+  </div>
+</div>
+  
+</body>
+</html>
