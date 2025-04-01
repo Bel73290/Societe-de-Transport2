ca874f7a6ebb4b4d3a04876c28fdd06405d3fb22
Dyx95 <aramaharivo73@gmail.com>
2025-04-01 15:59:21 +0200

    modif lien

--- livreur_main.php
+++ livreur_main.php
@@ -36,7 +36,7 @@ include "../db/db_disconnect.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fiche de tournée</title>
-    <link rel="stylesheet" href="livreur_style.css">
+    <link rel="stylesheet" href="css/livreur_style.css">
 </head>
 <body>
     <header>
