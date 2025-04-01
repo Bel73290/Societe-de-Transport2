fc261727fb4840eb32a25a9f7a0e937d6b0f2130
Dyx95 <aramaharivo73@gmail.com>
2025-04-01 15:12:48 +0200

    modif admin

--- Admin/admin.php
+++ Admin/admin.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calendrier Administrateur</title>
-    <link rel="stylesheet" href="css/admin_style.css">
+    <link rel="stylesheet" href="../css/admin_style.css">
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
 </head>
