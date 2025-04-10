fac688338c0eff1405bce09537ef14594f0cddc3
BRAVET <antobravet@gmail.com>
2025-04-10 17:05:03 +0200

    modif final fichier excel si tout marche

--- import/Excel.php
+++ import/Excel.php
@@ -27,7 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['excelFile'])) {
 
         array_shift($rows); // Supprime la ligne d'en-tête
 
-        $query = "INSERT INTO Utilisateur (nom, email, mot_de_passe, telephone, adresse, type) VALUES (?, ?, ?, ?, ?, 'client')";
+        $query = "INSERT INTO Utilisateur (nom, email, mdp, telephone, adresse, type) VALUES (?, ?, ?, ?, ?, 'client')";
         $stmt = mysqli_prepare($conn, $query);
 
         if (!$stmt) {
@@ -37,11 +37,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['excelFile'])) {
         foreach ($rows as $row) {
             $nom = $row[0];
             $email = $row[1];
-            $mot_de_passe = $row[2]; 
+            $mdp = $row[2]; 
             $telephone = $row[3];
             $adresse = $row[4];
 
-            mysqli_stmt_bind_param($stmt, "sssss", $nom, $email, $mot_de_passe, $telephone, $adresse);
+            mysqli_stmt_bind_param($stmt, "sssss", $nom, $email, $mdp, $telephone, $adresse);
             mysqli_stmt_execute($stmt);
         }
 
