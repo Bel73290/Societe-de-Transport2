1e1f2f5648177b9b57aa7e168a17b231af283cd7
Ilias Bellir <bellirilias@gmail.com>
2025-04-15 13:20:54 +0200

    TEST DATE MOIS

--- client.php
+++ client.php
@@ -133,7 +133,7 @@ $nextYear = $month == 12 ? $year + 1 : $year;
 
 // Si le mois est invalide (en dehors de la plage de 1 à 12), utilisez le mois actuel
 if ($month < 1 || $month > 12) {
-    $month = date('m');
+    $month = 4;
 }
 
 $moisFrancais = [
