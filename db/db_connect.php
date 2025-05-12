2ff58839a5af942b1fb09e51ca6c24a0a25e4b1f
DayiZ73 <ziyad.messaouddjebara@gmail.com>
2025-05-12 13:52:49 +0200

    modif ayoub

--- db/db_connect.php
+++ db/db_connect.php
@@ -4,8 +4,8 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 // 1- Connexion à la base de données
-//$conn = mysqli_connect("localhost", "info1", "1G1", "info1");
-$conn = mysqli_connect("localhost", "root", "", "info1");
+$conn = mysqli_connect("localhost", "info1", "1G1", "info1");
+//$conn = mysqli_connect("localhost", "root", "", "info1");
 mysqli_set_charset($conn, "utf8");
 
 // Vérification de la connexion
