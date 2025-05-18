2962675f7571efed13342b3700429a414335c576
Bel73290 <bellirilias@gmail.com>
2025-05-18 22:30:47 +0200

    changement du mois

--- client.php
+++ client.php
@@ -95,7 +95,8 @@ function generateCalendar($month, $year, $days) {
         }
         $isoDate = "$year-" . str_pad($month, 2, "0", STR_PAD_LEFT) . "-" . str_pad($currentDay, 2, "0", STR_PAD_LEFT);
         if ($currentDay < $days) {
-            $calendar .= "<td><span class='date-prec'>$currentDay</span></td>"; 
+            $calendar .= "<td><button class='date-prec' disabled>$currentDay</button></td>";
+
         } else {
             $calendar .= "<td><button class='date-btn' data-date='$isoDate'>$currentDay</button></td>"; 
 }
